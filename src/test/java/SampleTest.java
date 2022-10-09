860eae88ad377c0ebec6684f656f077cd18a3b26
Akash Krishna <akashkg@testvagrant.com>
2022-10-10 01:38:11 +0530

    Added First restAssured request

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,11 +1,22 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class SampleTest {
-    @Test(groups = "api")
-    public void shouldTest(){
-        Assert.assertEquals(1,1);
-        System.out.println("Test1");
+import static io.restassured.RestAssured.given;
 
+public class SampleTest {
+    @Test(groups = "api",description = "Should get all users")
+    public void shouldGetAllUsers(){
+
+     // 1. Arrange
+     // 2. Act
+     // 3. Assert
+
+        //can add mandatory header
+        given()
+                .when()
+                .get("https://gorest.co.in/public/v1/users")
+                .then()
+                .statusCode(200)
+                .log().body();
     }
 }
