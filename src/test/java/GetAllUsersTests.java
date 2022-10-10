914e5d53d0d325856b24de4b43c12e823e75059c
Akash Krishna <akashkg@testvagrant.com>
2022-10-10 11:41:47 +0530

    Added UUID.randomId() and refactored

--- src/test/java/GetAllUsersTests.java
+++ src/test/java/GetAllUsersTests.java
@@ -1,26 +1,36 @@
+import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import users.UsersClient;
 
 import static io.restassured.RestAssured.given;
 
 public class GetAllUsersTests {
+
+    // 1. Arrange
+    private UsersClient usersClient;
+
+    @BeforeClass
+    public void beforeClass(){
+        usersClient = new UsersClient();
+    }
     @Test(groups = "api",description = "Should get all users")
     public void shouldGetAllUsers(){
 
-     // 1. Arrange
      // 2. Act
-     // 3. Assert
+        usersClient
+                .getAllUsers()
+                    .then()
+                        .log().body()
 
-        //can add mandatory header
-        given()
-                .when()
-                    .get("https://gorest.co.in/public/v1/users")
-                .then()
-                    .log().body()
-                    .statusCode(200)
-                    .body("data",Matchers.hasSize(10))
-                    .body("data",Matchers.hasItem(Matchers.hasEntry("gender","male")));
+    // 3. Assert
+                        .statusCode(200)
+                        .body("data",Matchers.hasSize(10))
+                        .body("data",Matchers.hasItem(Matchers.hasEntry("gender","male")));
 
     }
+
+
 
 }
